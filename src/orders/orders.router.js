0a305bb89cea66bb5ca405d7c3ad6ddf7960bbda
NandoDz <nandodiaz388@gmail.com>
2021-04-15 20:56:29 -0700

    v0.1

---
+++ src/orders/orders.router.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const methodNotAllowed = require("../errors/methodNotAllowed");
+const controller = require("./orders.controller");
+
+const dishRouter = require("../dishes/dishes.router");
+
+// TODO: Implement the /orders routes needed to make the tests pass
+//router.route('/:orderId').all(methodNotAllowed)
+
+router
+  .route("/:orderId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.delete)
+  .all(methodNotAllowed);
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
+
+module.exports = router;
